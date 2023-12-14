a39ed4833faed2d82fe14c7f9a5d68e583f5d8a7
Paulo Gomes <paulocavalcante001@outlook.com>
2023-12-14 09:48:05 -0300

    chore: configurando github actions

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -7,14 +7,14 @@ const Breadcrumb = () => {
   let links: any[] = [
     {
       label: 'Página Inicial',
-      url: '/wbc/quickstarts/govbr-ds-wbc-quickstart-react/',
+      url: '/',
       home: true,
     },
   ]
 
-  if (location.pathname === '/wbc/quickstarts/govbr-ds-wbc-quickstart-react/formularios') label = 'Formulários'
-  if (location.pathname === '/wbc/quickstarts/govbr-ds-wbc-quickstart-react/sign-in') label = 'Sign-In'
-  if (location.pathname === '/wbc/quickstarts/govbr-ds-wbc-quickstart-react/cookiebar') label = 'Cookiebar'
+  if (location.pathname === '/formularios') label = 'Formulários'
+  if (location.pathname === '/sign-in') label = 'Sign-In'
+  if (location.pathname === '/cookiebar') label = 'Cookiebar'
 
   links.push({
     label: label,
@@ -22,7 +22,7 @@ const Breadcrumb = () => {
     active: true,
   })
 
-  if (location.pathname === '/wbc/quickstarts/govbr-ds-wbc-quickstart-react/') isHome = true
+  if (location.pathname === '/') isHome = true
 
   return !isHome ? <br-breadcrumb label="Breadcrumb" links={JSON.stringify(links)}></br-breadcrumb> : null
 }
