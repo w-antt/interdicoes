548803718030b3700bf62f44950278c85b170b22
Paulo Gomes <paulocavalcante001@outlook.com>
2023-12-14 10:01:03 -0300

    chore: roolback

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-// import '@govbr-ds/core/dist/core.min.css'
-// import '@govbr-ds/webcomponents/dist/webcomponents.umd.min.js'
+import '@govbr-ds/core/dist/core.min.css'
+import '@govbr-ds/webcomponents/dist/webcomponents.umd.min.js'
 
 const App = () => {
   return (
-    <p>Teste pipeline</p>
-    // <>
-    //   <div className="template-base">
-    //     <Header />
-    //     <main className="d-flex flex-fill mb-5" id="main">
-    //       <div className="container-fluid">
-    //         <div className="row">
-    //           <div className="col-sm-4 col-lg-3">
-    //             <Menu />
-    //           </div>
-    //           <div className="col mb-5">
-    //             <Breadcrumb />
-    //             <div className="main-content pl-sm-3 mt-4" id="main-content">
-    //               <Routes>
-    //                 <Route path="/" element={<HomePage />} />
-    //                 <Route path="/formularios" element={<FormPage />} />
-    //                 <Route path="/sign-in" element={<SignInPage />} />
-    //                 <Route path="/cookiebar" element={<CookiebarPage />} />
-    //               </Routes>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </main>
-    //     <Footer />
-    //   </div>
-    // </>
+    <>
+      <div className="template-base">
+        <Header />
+        <main className="d-flex flex-fill mb-5" id="main">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-sm-4 col-lg-3">
+                <Menu />
+              </div>
+              <div className="col mb-5">
+                <Breadcrumb />
+                <div className="main-content pl-sm-3 mt-4" id="main-content">
+                  <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/formularios" element={<FormPage />} />
+                    <Route path="/sign-in" element={<SignInPage />} />
+                    <Route path="/cookiebar" element={<CookiebarPage />} />
+                  </Routes>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </>
   )
 }
 
